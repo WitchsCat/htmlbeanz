cc4081b380b60a51d7678aaacb369cb024b4b417
velxeor <david.lucky.star@gmail.com>
2012-11-22 18:59:44 +0400

    add new Rooms - NiceRoom and BadRoom

---
+++ src/main/java/org/funcode/htmlbeans/samples/foundation/BadRoom.java
@@ -0,0 +1,21 @@
+package org.funcode.htmlbeans.samples.foundation;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: amatveev
+ * Date: 22.11.12
+ * Time: 18:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class BadRoom extends Room {
+
+    private int howBad;
+
+    public int getHowBad() {
+        return howBad;
+    }
+
+    public void setHowBad(int howBad) {
+        this.howBad = howBad;
+    }
+}
